24f96bee3b123ce0615f31e2e5be638cac9e619c
Yug Sarda <2023ucp1663@mnit.ac.in>
2024-10-15 14:51:56 +0530

    hello

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+
+a = 5
+b = 7
+
+#<<<<<<< HEAD
+#print(np.dot(a, b))
+#I am making changes in the main branch
+#=======
+#print(np.dot(a, b))
+output=np.dot(a, b)
+print(output)
+#this is done using numpy pandas
+#hello
+#wrld
+#>>>>>>> parallel
+
